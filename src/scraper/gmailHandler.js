7e7bbb6f072035866e5475951fc1735c57177b5a
Pike <56088716+PikeNote@users.noreply.github.com>
2024-10-09 17:16:51 -0400

    Switch to ImapFlow
    - Switch to a more update to date imap module for more security

--- src/scraper/gmailHandler.js
+++ src/scraper/gmailHandler.js
@@ -2,69 +2,43 @@
 
 const verificationCodeRegex = /The sign-in code you requested for CampusGroups is ([0-9]{6})/i
 require('dotenv').config();
-const Imap = require('imap')
+const { ImapFlow } = require('imapflow');
 
-let mailServer1 = new Imap({
-  user: process.env.GMAIL_EMAIL,
-  password: process.env.GMAIL_APP_PASSWORD,
+const client = new ImapFlow({
   host: 'imap.gmail.com',
   port: 993,
-  tls: true,
-  tlsOptions: {
-    rejectUnauthorized: false
+  secure: true,
+  auth: {
+      user: myMail,
+      pass: myPwd
   },
-  authTimeout: 3000,
-  keepalive: true
-}).once('error', function (err) {
-  console.log('Source Server Error:- ', err);
+  logger: false
 });
 
-mailServer1.once('ready', async function () {
-  console.log('Mail Server Ready');
-});
 
 async function getVerificationCode(delay) {
   await new Promise(r => setTimeout(r, delay));
-  await mailServer1.connect();
-  mailServer1.once('ready', async function () {
-    let code = await getInboxCode(mailServer1);
-    await mailServer1.end();
-    return code;
-  });
+  let returnCode = 0;
+  await client.connect();
+
+  let lock = await client.getMailboxLock('INBOX');
+  try {
+      let message = await client.fetchOne(client.mailbox.exists, { source: true });
+      let textmsg = message.source.toString();
+
+      var codeRegex = textmsg.match(verificationCodeRegex);
+      if(codeRegex && codeRegex.length > 1) {
+        returnCode = codeRegex[1];
+      }
+  } finally {
+      lock.release();
+  }
+
+  await client.logout();
+  return returnCode;
 }
 
-async function getInboxCode () {
-  return new Promise((resolve, reject) => {
-    mailServer1.openBox('INBOX', true, (err, box) => {
 
-      if (err) throw err;
-      var f = mailServer1.seq.fetch(box.messages.total + ':*', { bodies: ['HEADER.FIELDS (FROM)','TEXT'] });
-      f.on('message', function(msg, seqno) {
-        msg.on('body', function(stream, info) {
-          var buffer = '', count = 0;
-          stream.on('data', function(chunk) {
-            count += chunk.length;
-            buffer += chunk.toString('utf8');
-          });
-          stream.once('end', async function() {
-            if (info.which === 'TEXT')  {
-              var codeRegex = buffer.toString().match(verificationCodeRegex);
-              if(codeRegex && codeRegex.length > 1) {
-                resolve(codeRegex[1]);
-              } else {
-                resolve(0);
-              }
-            }
-          });
-        });
-      });
-      f.once('error', function(err) {
-        reject(err);
-        console.log('Fetch error: ' + err);
-      });
-    });
-  });
-}
 
 /*
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
